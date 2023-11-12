cff13c71d3adffb0d043797144f894105610bcd4
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-11-12 17:06:24 -0500

    create login view and route

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,7 @@
+import 'package:my_scrum/meta/views/auth/login_view.dart';
+
+class AppRoutes {
+  static const String LoginRoute = '/login';
+
+  static final routes = {LoginRoute: (context) => LoginView()};
+}
