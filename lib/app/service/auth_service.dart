53051fe5f5fb9e32661ea6e21f94fd712f7250b5
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-11-13 11:14:09 -0500

    create function for sign up and login

--- lib/app/service/auth_service.dart
+++ lib/app/service/auth_service.dart
@@ -5,5 +5,27 @@ class AuthenticationService {
   Future<void> signUp({required String email, required String password}) async {
     GotrueSessionResponse response =
         await SupabaseCredentials.supabaseClient.auth.signUp(email, password);
+
+    if (response.error == null) {
+      String? userEmail = response.data!.user!.email;
+      print("Registro exitoso $userEmail");
+    } else {
+      print("Registro invalido ${response.error!.message}");
+    }
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    GotrueSessionResponse response =
+        await SupabaseCredentials.supabaseClient.auth.signIn(
+            email: email,
+            password: password,
+            options: AuthOptions(redirectTo: SupabaseCredentials.apiurl));
+
+    if (response.error == null) {
+      String? userEmail = response.data!.user!.email;
+      print("Ingreso exitoso $userEmail");
+    } else {
+      print("Ingreso invalido ${response.error!.message}");
+    }
   }
 }
